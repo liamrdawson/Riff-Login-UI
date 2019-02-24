5866e936ba6a51a702856766b8ffb32ae1e89cc2
Liam Dawson <liamrdawson@gmail.com>
2019-02-24 18:02:35 +0000

    Add UI animation on log In 'click' event

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const button = document.getElementById('button');
+const form = document.getElementById('form');
+
+addEventListener('click', (e) => {
+    let buttonText = document.getElementById('loginText');
+    if(e.target.tagName === 'BUTTON' ||  e.target == buttonText) {
+        
+        
+        input1 = document.getElementById('down-1');
+        input2 = document.getElementById('down-2');
+
+        button.classList.add('extend');
+        buttonText.classList.add('fade');
+
+        input1.classList.add('fade-in');
+        input2.classList.add('dropdown');
+        
+        button.classList.add('re-button');
+
+    }
+});
+
