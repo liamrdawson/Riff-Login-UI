0b9ef316fdc1f1fccfdffb8eb0c5bea4855935a0
Liam Dawson <liamrdawson@gmail.com>
2019-02-24 20:25:09 +0000

    add placeholders for input fields

--- js/script.js
+++ js/script.js
@@ -1,22 +1,21 @@
 const button = document.getElementById('button');
 const form = document.getElementById('form');
+const userEmail = document.getElementById('user_email');
+const userPassword = document.getElementById('user_password');
 
 addEventListener('click', (e) => {
     let buttonText = document.getElementById('loginText');
     if(e.target.tagName === 'BUTTON' ||  e.target == buttonText) {
-        
-        
         input1 = document.getElementById('down-1');
         input2 = document.getElementById('down-2');
-
         button.classList.add('extend');
         buttonText.classList.add('fade');
-
         input1.classList.add('fade-in');
         input2.classList.add('dropdown');
-        
         button.classList.add('re-button');
-
     }
+
 });
+
+
 
